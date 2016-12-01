49de1be228c3002af4cdcc2178b8710d2ce0b5ff
Don Robles <tinkerthinker@outlook.com>
2016-12-01 15:16:17 -0800

    Began adding routes and laying out some basic HTML layouts.

--- client/main.js
+++ client/main.js
@@ -1,20 +1,20 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
+import './partials/Hello.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.Hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.Hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.Hello.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
