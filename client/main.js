ca857e08555224cbb843e63f45d1b6cd290f7243
Don Robles <tinkerthinker@outlook.com>
2017-01-28 12:42:48 -0800

    Removed a bunch of code that was cluttering my thinking.

--- client/main.js
+++ client/main.js
@@ -1,56 +1,48 @@
 import {Template} from "meteor/templating";
 import {Meteor} from "meteor/meteor";
 
-import "./layout/partials/Login.html";
-
 (function () {
     "use strict";
 
-    Template.Login.onCreated(function loginOnCreated() {
-        return 0;
-    });
-
-    Template.Login.helpers({});
-
-    Template.Login.events({});
-
     Template.onlyIfLoggedIn.helpers({
-        authInProcess: function() {
+        authInProcess: function () {
             return Meteor.loggingIn();
         },
-        canShow: function() {
+        canShow: function () {
             return !!Meteor.user();
         }
     });
 
-    Template.editPost.onCreated(function() {
-        var self = this;
-        self.postSubReady = new ReactiveVar();
-        self.autorun(function() {
-            var postId = FlowRouter.getParam("postId");
-            var subHandle = self.subscribe("singlePost", postId);
-            self.postSubReady.set(subHandle.ready());
-        });
-    });
-
     Template.editPost.helpers({
-        authInProcess: function() {
+        authInProcess: function () {
             var postSubReady = Template.instance().postSubReady.get();
             return Meteor.loggingIn() || !postSubReady;
         },
-        canShow: function() {
+        canShow: function () {
             var user = Meteor.user();
-            if(!user) {
+            if (!user) {
                 return false;
             }
 
             var postId = FlowRouter.getParam("postId");
             var post = Posts.findOne({_id: postId});
-            if(!post) {
+            if (!post) {
                 return false;
             }
 
             return post.owner === user._id;
+        }
+    });
+
+    Template.MainLanding.helpers({
+        isCustomer: function () {
+            return false;
+        },
+        isAdmin: function () {
+            return false;
+        },
+        isSuperAdmin: function () {
+            return false;
         }
     });
 }());
