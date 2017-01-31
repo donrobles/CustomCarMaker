685a49077a00072995b9bb1d4db0316ece76f812
Don Robles <tinkerthinker@outlook.com>
2017-01-30 19:58:58 -0800

    Removed commented-out code.

--- client/main.js
+++ client/main.js
@@ -3,36 +3,6 @@ import {Template} from "meteor/templating";
 (function () {
     "use strict";
 
-    /*Template.onlyIfLoggedIn.helpers({
-     authInProcess: function () {
-     return Meteor.loggingIn();
-     },
-     canShow: function () {
-     return !!Meteor.user();
-     }
-     });
-
-     Template.editPost.helpers({
-     authInProcess: function () {
-     var postSubReady = Template.instance().postSubReady.get();
-     return Meteor.loggingIn() || !postSubReady;
-     },
-     canShow: function () {
-     var user = Meteor.user();
-     if (!user) {
-     return false;
-     }
-
-     var postId = FlowRouter.getParam("postId");
-     var post = Posts.findOne({_id: postId});
-     if (!post) {
-     return false;
-     }
-
-     return post.owner === user._id;
-     }
-     });*/
-
     Template.Welcome.helpers({
         isCustomer: function () {
             return false;
@@ -45,7 +15,6 @@ import {Template} from "meteor/templating";
         }
     });
 
-    
     Template.index.onCreated(function () {
         this.currentTab = new ReactiveVar("books");
     });
