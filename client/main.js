f0657ca08ba984f86b617cf29f5d5458aa362586
Don Robles <tinkerthinker@outlook.com>
2017-01-30 19:57:36 -0800

    Committing code on how dynamic templates can be used.

--- client/main.js
+++ client/main.js
@@ -1,40 +1,39 @@
 import {Template} from "meteor/templating";
-import {Meteor} from "meteor/meteor";
 
 (function () {
     "use strict";
 
-    Template.onlyIfLoggedIn.helpers({
-        authInProcess: function () {
-            return Meteor.loggingIn();
-        },
-        canShow: function () {
-            return !!Meteor.user();
-        }
-    });
+    /*Template.onlyIfLoggedIn.helpers({
+     authInProcess: function () {
+     return Meteor.loggingIn();
+     },
+     canShow: function () {
+     return !!Meteor.user();
+     }
+     });
 
-    Template.editPost.helpers({
-        authInProcess: function () {
-            var postSubReady = Template.instance().postSubReady.get();
-            return Meteor.loggingIn() || !postSubReady;
-        },
-        canShow: function () {
-            var user = Meteor.user();
-            if (!user) {
-                return false;
-            }
+     Template.editPost.helpers({
+     authInProcess: function () {
+     var postSubReady = Template.instance().postSubReady.get();
+     return Meteor.loggingIn() || !postSubReady;
+     },
+     canShow: function () {
+     var user = Meteor.user();
+     if (!user) {
+     return false;
+     }
 
-            var postId = FlowRouter.getParam("postId");
-            var post = Posts.findOne({_id: postId});
-            if (!post) {
-                return false;
-            }
+     var postId = FlowRouter.getParam("postId");
+     var post = Posts.findOne({_id: postId});
+     if (!post) {
+     return false;
+     }
 
-            return post.owner === user._id;
-        }
-    });
+     return post.owner === user._id;
+     }
+     });*/
 
-    Template.MainLanding.helpers({
+    Template.Welcome.helpers({
         isCustomer: function () {
             return false;
         },
@@ -43,6 +42,50 @@ import {Meteor} from "meteor/meteor";
         },
         isSuperAdmin: function () {
             return false;
+        }
+    });
+
+    
+    Template.index.onCreated(function () {
+        this.currentTab = new ReactiveVar("books");
+    });
+
+    Template.index.helpers({
+        tab: function () {
+            return Template.instance().currentTab.get();
+        },
+        tabData: function () {
+            let tab = Template.instance().currentTab.get();
+
+            let data = {
+                "books": [
+                    {"name": "Seeking Wisdom: From Darwin to Munger", "creator": "Peter Bevelin"},
+                    {"name": "Hatchet", "creator": "Some Guy"},
+
+                ],
+                "movies": [
+                    {"name": "Ghostbusters", "creator": "Dan Aykroyd"},
+                    {"name": "Inception", "creator": "Chris Nolan"}
+
+                ],
+                "games": [
+                    {"name": "Grand Theft Auto V", "creator": "Rockstar Games"},
+                    {"name": "Tetris", "creator": "Tetris Guy"}
+                ]
+            };
+
+            return {contentType: tab, items: data[tab]};
+        },
+    });
+
+    Template.index.events({
+        "click .nav-pills li": function (event, template) {
+            let clickedTab = $(event.target).closest("li");
+
+            clickedTab.addClass("active");
+            $(".nav-pills li").not(clickedTab).removeClass("active");
+
+            template.currentTab.set(clickedTab.data("template"));
         }
     });
 }());
