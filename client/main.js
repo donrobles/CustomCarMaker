f3eaa3a822a1ae3f6257ef5387fabbdb94974bbb
Don Robles <tinkerthinker@outlook.com>
2017-01-30 20:12:23 -0800

    Added comments to main.js

--- client/main.js
+++ client/main.js
@@ -15,12 +15,15 @@ import {Template} from "meteor/templating";
         }
     });
 
+    /*Code for learning more about dynamic templates.*/
     Template.index.onCreated(function () {
+        //Create ReactiveVar to track the currently selected template.
         this.currentTab = new ReactiveVar("books");
     });
 
     Template.index.helpers({
         tab: function () {
+            //Return the current tab set to the ReactiveVar
             return Template.instance().currentTab.get();
         },
         tabData: function () {
@@ -43,17 +46,24 @@ import {Template} from "meteor/templating";
                 ]
             };
 
+            //Return an object with set "contentType" and "items" variables.
             return {contentType: tab, items: data[tab]};
         },
     });
 
     Template.index.events({
+        //Click event for when one of the tabs is clicked.
         "click .nav-pills li": function (event, template) {
+            //Grab the clicked tab.
             let clickedTab = $(event.target).closest("li");
 
+            //Add the "active" class to the clicked tab.
             clickedTab.addClass("active");
+            //Remove the "active" class from the tabs that AREN'T the clicked tab.
             $(".nav-pills li").not(clickedTab).removeClass("active");
 
+            /*Set the Reactive Var "currentTab" to the clicked tab, setting the
+             Dynamic template "data" variable to template of the clicked tab.*/
             template.currentTab.set(clickedTab.data("template"));
         }
     });
